7f7acdacba0a99b5658cbc7ba1c587edfcb3ef2d
MungC2010G2 <zhaiduong26z@gmail.com>
2021-02-01 17:56:45 +0700

    add app từ điển

---
+++ src/main/java/DictionaryServlet.java
@@ -0,0 +1,35 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "DictionaryServlet",urlPatterns = "/translate")
+public class DictionaryServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    Map<String, String> dictionary = new HashMap<>();
+    dictionary.put("helle","xin chào");
+    dictionary.put("how", "thế nào");
+    dictionary.put("book","quyển vở");
+    String search = request.getParameter("txtSearch");
+    PrintWriter writer = response.getWriter();
+    writer.println("<html>");
+    String  result = dictionary.get(search);
+    if(result != null){
+        writer.println("Word: " + search);
+        writer.println("result: " + result);
+    } else {
+        writer.println("not found");
+    }
+    writer.println("</html>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
